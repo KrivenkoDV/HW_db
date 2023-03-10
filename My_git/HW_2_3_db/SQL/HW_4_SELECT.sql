5a21ea4f99fbe0483cfd0194c8951f25a2049930
Dmitriy Krivenko <timonk@mail.ru>
2023-03-10 16:17:15 +0300

    Отработка замечаний в HW_4_SELECT

--- My_git/HW_2_3_db/SQL/HW_4_SELECT.sql
+++ My_git/HW_2_3_db/SQL/HW_4_SELECT.sql
@@ -16,11 +16,10 @@ join mus_alboms ma on ma.id = mt.mus_alboms_id
 group by ma.name 
 
 -- все исполнители, которые не выпустили альбомы в 2020 году;
-select m.name, ma.name name from musicians m 
+select m.name, ma.name albom from musicians m 
 join links_alboms la on musicians_id = m.id
 join mus_alboms ma on mus_alboms_id = ma.id
 where ma.year <> '2020'
-group by m.name, ma.name 
 
 -- названия сборников, в которых присутствует конкретный исполнитель (выберите сами);
 select c.name from collections c 
@@ -30,7 +29,6 @@ join mus_alboms ma on ma.id = mt.mus_alboms_id
 join links_alboms la on la.mus_alboms_id = ma.id 
 join musicians m on m.id = la.musicians_id 
 where m.name = 'Максим' 
-group by c.name
 
 -- название альбомов, в которых присутствуют исполнители более 1 жанра;
 select ma.name, count(mg.name) from mus_alboms ma 
@@ -53,16 +51,12 @@ join mus_alboms ma on ma.id = mt.mus_alboms_id
 join links_alboms la on la.mus_alboms_id = ma.id 
 join musicians m on m.id = la.musicians_id 
 where time_track = (select min(time_track) from mus_tracks)
-group by m.name, time_track
 
 -- название альбомов, содержащих наименьшее количество треков.
 select ma.name, count(mus_alboms_id) tracks from mus_alboms ma
 join mus_tracks mt on mt.mus_alboms_id = ma.id
-group by ma.name, mt.mus_alboms_id
-having  count(mus_alboms_id) <= 1
+group by ma.name
+having  count(mus_alboms_id) = (select min(mus_alboms_id) tracks from mus_alboms ma join mus_tracks mt on mt.mus_alboms_id = ma.id) --group by min(mus_alboms_id) 
+order by min(mus_alboms_id)
+limit 1
 
-select m.name, ma.name albom, mt.name song from musicians m
-join links_alboms la on la.musicians_id = m.id 
-join mus_alboms ma on ma.id = la.mus_alboms_id
-join mus_tracks mt on mt.mus_alboms_id = ma.id 
-group by m.name, ma.name, mt.name
